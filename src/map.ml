9fe68e4f5cfb2bc19f187a9908356993d6a6ab11
saysayo <sayobamigbade@gmail.com>
2022-10-04 17:16:32 +0100

    refactored code

--- src/map.ml
+++ src/map.ml
@@ -1,74 +1,52 @@
 open Leaflet
 open Brr
 
-let map = 
-  let map_id = (Document.find_el_by_id G.document) (Jstr.v "map") in 
-    match map_id with 
-      | Some map_id -> Map.create map_id
-      | None -> Map.of_jv (Jv.of_error (Jv.Error.v (Jstr.v "Map ID not found")))
+type entry = { latitude : float; longitude : float; description : string }
 
-let () = 
+let lagos =
+  { latitude = 6.465422; longitude = 3.406448; description = "This is Lagos" }
+
+let paris =
+  { latitude = 48.864716; longitude = 2.349014; description = "This is Paris" }
+
+let spain =
+  { latitude = 39.466667; longitude = -0.375000; description = "This is Spain" }
+
+let denmark =
+  { latitude = 56.2639; longitude = 9.5018; description = "This is Denmark" }
+
+let list_of_locations = [lagos; paris; spain; denmark]
+
+let map =
+  let map_id = (Document.find_el_by_id G.document) (Jstr.v "map") in
+  match map_id with
+  | Some map_id -> Map.create map_id
+  | None -> Map.of_jv (Jv.of_error (Jv.Error.v (Jstr.v "Map ID not found")))
+
+let () =
   let lat_lng = Latlng.create 51.505 (-0.09) in
   let zoom = Some 13 in
   Map.set_view lat_lng ~zoom map
 
-  (* Tile layer *)
+(* Tile layer *)
 let osm_layer =
-  let url = Some("https://tile.openstreetmap.org/{z}/{x}/{y}.png") in  
-    Layer.create_tile_osm url
+  let url = Some "https://tile.openstreetmap.org/{z}/{x}/{y}.png" in
+  Layer.create_tile_osm url
 
 let () = Layer.add_to map osm_layer
 
-(* Lagos Marker *)
-let lagos_marker = 
-  let lat_lng = Latlng.create 6.465422 3.406448 in
-  Layer.create_marker lat_lng 
+let create_marker location =
+  let latitude = location.latitude in
+  let longitude = location.longitude in
+  let popup_string = location.description in
+  let marker =
+    let lat_lng = Latlng.create latitude longitude in
+    Layer.create_marker lat_lng
+  in
+  let () = Layer.add_to map marker in
+  let str = El.txt' popup_string in
+  let () = Layer.bind_popup str marker in
+  Layer.open_popup marker
 
-let () = Layer.add_to map lagos_marker
-
-(* Lagos Popup *)
-let () = 
-  let str = El.txt' "This is Lagos" in
-  let () = Layer.bind_popup str lagos_marker in
-  Layer.open_popup lagos_marker
-
-(* Paris Marker *)
-let paris_marker = 
-  let lat_lng = Latlng.create 48.864716 2.349014 in
-  Layer.create_marker lat_lng 
-
-let () = Layer.add_to map paris_marker
-
-(* Paris Popup *)
-let () = 
-  let str = El.txt' "This is Paris" in
-  let () = Layer.bind_popup str paris_marker in
-  Layer.open_popup paris_marker
-
-(* Spain Marker *)
-let spain_marker = 
-  let lat_lng = Latlng.create 39.466667 (-0.375000) in
-  Layer.create_marker lat_lng 
-
-let () = Layer.add_to map spain_marker
-
-(* Spain Popup *)
-let () = 
-  let str = El.txt' "This is Valencia" in
-  let () = Layer.bind_popup str spain_marker in
-  Layer.open_popup spain_marker
-
-  (* Denmark Marker *)
-let denmark_marker = 
-  let lat_lng = Latlng.create 56.2639 9.5018 in
-  Layer.create_marker lat_lng 
-
-let () = Layer.add_to map denmark_marker
-
-(* Denmark Popup *)
-let () = 
-  let str = El.txt' "This is Denmark" in
-  let () = Layer.bind_popup str denmark_marker in
-  Layer.open_popup denmark_marker
-
+let () = List.iter create_marker list_of_locations
 
