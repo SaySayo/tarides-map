c4f64506b2fdf02805342d0d85ac36d5f3ff1a8d
saysayo <sayobamigbade@gmail.com>
2022-10-20 15:13:04 +0100

    added yojson

--- server/server.eml.ml
+++ server/server.eml.ml
@@ -1,9 +1,25 @@
+open Yojson
+
+type message_object = {
+  message : string;
+} [@@deriving yojson]
+
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
     
-    Dream.get "/src/htdocs/data/location.json"
-      (fun _ ->
-        Dream.json "/src/htdocs/data/location.json"); 
+    Dream.post "/src/htdocs/data/location.json"
+    (fun request ->
+      let%lwt body = Dream.body request in
+
+      let message_object =
+        body
+        |> Yojson.Safe.from_string
+        |> message_object_of_yojson
+      in
+
+      `String message_object.message
+      |> Yojson.Safe.to_string
+      |> Dream.json);
       ]
