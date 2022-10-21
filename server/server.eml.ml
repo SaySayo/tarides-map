cdedd1d3a428c655308a988a4872663024abcaa5
saysayo <sayobamigbade@gmail.com>
2022-10-21 10:31:44 +0100

    getting json in dream server

--- server/server.eml.ml
+++ server/server.eml.ml
@@ -1,21 +1,11 @@
-type entry = { latitude : float; longitude : float; description : string } [@@deriving yojson]
-
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
     
-    Dream.post "/src/htdocs/data/location.json"
-    (fun request ->
-      let%lwt body = Dream.body request in
-
-      let entry_object =
-        body
-        |> Yojson.Safe.from_string
-        |> entry_of_yojson
-      in
-
-      `String entry_object.description
-      |> Yojson.Safe.to_string
-      |> Dream.json);
+    Dream.get "/src/htdocs/data/location.json"
+    (fun _ ->
+      let%lwt body = Lwt_io.open_file ~mode:Input "/src/htdocs/data/location.json" in
+      let%lwt body_to_string = Lwt_io.read body in
+        Dream.json body_to_string)
       ]
